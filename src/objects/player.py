d37292144b7dbe667854ff6075d4ae530271100c
Anthony de Cruz <anthonydaviddecruz@gmail.com>
2021-12-21 13:49:07 +0000

    Added player sprite and player movement

--- src/objects/player.py
+++ src/objects/player.py
@@ -1,13 +1,59 @@
+import math
+
 from objects import game_object
 import pygame
 
-class Player(game_object.GameObject):
 
+class Player(game_object.GameObject):
     def __init__(
-        self, 
-        image: pygame.Surface, 
-        x_pos: int,
-        y_pos: int,
-        *group: pygame.sprite.Group):
+        self, image: pygame.Surface, x_pos: int, y_pos: int, *group: pygame.sprite.Group
+    ):
 
         super().__init__(x_pos, y_pos, *group, image=image)
+
+        # Save original image to preserve quality after transformations
+        self.original_image = image
+        self.original_rect = self.rect
+
+        self.movement_speed = 5
+        self.angle = 0
+        self.rotation_speed = 5
+
+    def move(self, forward, sideways) -> None:
+
+        """Calculate and apply movement."""
+
+        # Calculate the x and y components of the movement, multiply by movement speed
+        # multiplier and directional modifer
+        if forward != 0:
+            self.rect.x -= (
+                math.sin(math.radians(self.angle)) * self.movement_speed * forward
+            )
+            self.rect.y -= (
+                math.cos(math.radians(self.angle)) * self.movement_speed * forward
+            )
+        if sideways != 0:
+            self.rect.x += (
+                math.cos(math.radians(self.angle)) * self.movement_speed * sideways
+            )
+            self.rect.y -= (
+                math.sin(math.radians(self.angle)) * self.movement_speed * sideways
+            )
+
+    def rotate(self, direction) -> None:
+
+        """Calculate and apply changes in angle."""
+
+        # Change angle depending on the direction and speed multiplier
+        self.angle += direction * self.rotation_speed
+
+        # Ensure that angle doesn't go above 360 to keep angles sane
+        if self.angle > 360:
+            self.angle -= 360
+        elif self.angle < -360:
+            self.angle += 360
+
+        # Create a new image by rotating the original image by new angle
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        # Create a new rect based on the center of the old rect
+        self.rect = self.image.get_rect(center=self.rect.center)
--- src/scenes/raycast_demo.py
+++ src/scenes/raycast_demo.py
@@ -1,14 +1,16 @@
+import os
+
 from settings import SETTINGS
 from constants import COLOURS
+from objects import player
 import pygame
 
 
-class Object():
+class Object:
     pass
 
 
 class RaycastDemo(pygame.sprite.Sprite):
-
     def __init__(self, *group):
 
         super().__init__(*group)
@@ -18,7 +20,7 @@ class RaycastDemo(pygame.sprite.Sprite):
         self.image = pygame.display.get_surface()
         self.rect = self.image.get_rect()
 
-        self.width = self.image.get_width() 
+        self.width = self.image.get_width()
         self.height = self.image.get_height()
 
         ## Create sprite groups
@@ -27,23 +29,44 @@ class RaycastDemo(pygame.sprite.Sprite):
         self.group.map = pygame.sprite.Group()
 
         ## Load assets
-
+        # Temporary surface, will load an actual sprite asset in the future
+        player_img = pygame.image.load(os.path.join("assets", "Arrow.png"))
 
         ## Create objects
-
+        # Player
+        self.player = player.Player(player_img, 475, 200, self.group.player)
+        print(self.player.rect)
 
     def update(self, event_list) -> None:
 
         """Method to run every frame and update scene behaviour."""
-        
-        #//for event in event_list:
-        #//    pass
 
-        pass
-    
+        keys = pygame.key.get_pressed()
+
+        if True in keys:
+
+            if (
+                keys[pygame.K_w]
+                or keys[pygame.K_a]
+                or keys[pygame.K_s]
+                or keys[pygame.K_d]
+            ):
+                self.group.player.sprites()[0].move(
+                    (keys[pygame.K_w] - keys[pygame.K_s]),
+                    (keys[pygame.K_d] - keys[pygame.K_a]),
+                )
+
+            if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+                self.group.player.sprites()[0].rotate(
+                    (keys[pygame.K_LEFT] - keys[pygame.K_RIGHT])
+                )
+
+        # for event in event_list:
+
+        #    pass
 
     def render(self) -> pygame.Surface:
-        
+
         """Creates and returns a surface of the scene to be drawn.
 
         Returns:
@@ -53,10 +76,7 @@ class RaycastDemo(pygame.sprite.Sprite):
         # Background
         self.image.fill(COLOURS["Dark Grey"])
 
-        pygame.draw.rect(self.image, COLOURS["White"], (50,50,10,10))
-
         # Draw sprite group
-        
+        self.group.player.draw(self.image)
 
         return self.image
-
